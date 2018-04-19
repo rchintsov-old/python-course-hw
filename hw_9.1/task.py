d3e46b4793c55b638d212f78e4323a9953c99393
Ruslan Chintsov <r.chintsov@gmail.com>
2018-04-19 18:14:48 +0300

    task

--- hw_9.1/task.py
+++ hw_9.1/task.py
@@ -2,27 +2,62 @@ import time
 from datetime import datetime
 
 class Logger:
+    """
+    Контекстный менеджер, логгирующий ошибки в файл.
+    Не скрывает ошибку, пробрасывает её дальше.
+    Если ошибка не произошла, файл не создает и ничего не записывает.
+
+    :param str filename: имя лог-файла, который будет создан.
+    :return: лог-файл.
+    :rtype: file
+
+    :Example:
+
+    >>> with Logger('log.txt'):
+    >>>     print(1)
+    # 1
+
+    >>> with Logger('log.txt'):
+    >>>    raise TypeError('something goes wrong')
+    # TypeError: something goes wrong
+    """
     def __init__(self, filename):
         self.start = 0
         self.filename = filename
 
+
     def __enter__(self):
         self.start = time.time()
-        self.file = open(self.filename, 'a')
         return self
 
+
     def __exit__(self, exc_type, exc_val, exc_tb):
-        execution_time = time.time() - self.start
-        self.file.write('{time} | {error}: {args} | {execution}'.format(
-            time = datetime.fromtimestamp(execution_time).strftime("%c"),
-            error = exc_type.__name__,
-            args = exc_val,
-            execution = execution_time
-        ))
-        self.file.close()
+        """
+        Если ошибка происходит, принимает параметры ошибки.
+        Сохраняет их в файл + время ошибки + время выполнения кода.
+        
+        :param class exc_type: класс ошибки.
+        :param Error exc_val: детали ошибки.
+        :return: файл.
+        :rtype: file
+        """
+
+        if exc_type:
+            execution_time = time.time() - self.start
+            with open(self.filename, 'a') as f:
+                f.write('{time} | {error}: {args} | {execution}'.format(
+                    time = datetime.fromtimestamp(execution_time).strftime("%c"),
+                    error = exc_type.__name__,
+                    args = exc_val,
+                    execution = execution_time
+                ))
 
 
 if __name__ == '__main__':
+
+    with Logger('log.txt'):
+        print(1)
+
     with Logger('log.txt'):
         raise TypeError('something goes wrong')
 
