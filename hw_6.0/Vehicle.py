5512bfcd3af940724861997c6f5b2e94e2eef40b
Ruslan Chintsov <r.chintsov@gmail.com>
2018-05-17 18:56:21 +0300

    fixed

--- hw_6.0/Vehicle.py
+++ hw_6.0/Vehicle.py
@@ -5,15 +5,8 @@ class Vehicle(metaclass=abc.ABCMeta):
     """
     Abstract class.
     """
-    def __init__(self):
-        self.base_cost = None
-
-    @abc.abstractmethod
-    def vehicle_type(self):
-        pass
-
     @staticmethod
-    def is_motorcycle(self, wheels):
+    def is_motorcycle(wheels):
         if wheels:
             if 1 < wheels <= 3:
                 return True
@@ -22,29 +15,30 @@ class Vehicle(metaclass=abc.ABCMeta):
         else:
             raise ValueError('vehicle should have a wheels')
 
-    @classmethod
-    def purchase_price(self, base_cost, km):
-        if self.base_cost != None:
-            return self.base_cost - 0.1 * km
-        else:
-            return base_cost - 0.1 * km
+
+    @abc.abstractmethod
+    def vehicle_type(self):
+        pass
+
+    @abc.abstractmethod
+    def purchase_price(self):
+        pass
 
     @abc.abstractmethod
     def vehicle_year(self):
         pass
 
-    @abc.abstractmethod
-    def model(self):
-        pass
-
 
 class Car(Vehicle):
-    def __init__(self, year, model, base_cost, passed_km):
+    """Car"""
+    def __init__(self, year, model, base_cost):
         super().__init__()
         self.base_cost = base_cost
-        self.passed_km = passed_km
-        self.year = year
-        self.model_ = model
+        self._year = year
+        self.model = model
+
+    def purchase_price(self, km):
+        return self.base_cost - 0.1 * km
 
     @property
     def vehicle_type(self):
@@ -52,41 +46,39 @@ class Car(Vehicle):
 
     @property
     def vehicle_year(self):
-        return self.year
-
-    @property
-    def model(self):
-        return self.model_
+        return self._year
 
 
 class Motorcycle(Vehicle):
-    def __init__(self, year, model, base_cost, passed_km):
+    """Motorcycle"""
+    def __init__(self, year, model, base_cost):
         super().__init__()
         self.base_cost = base_cost
-        self.passed_km = passed_km
-        self.year = year
-        self.model_ = model
-
+        self._year = year
+        self.model = model
+    
+    def purchase_price(self, km):
+        return self.base_cost - 0.1 * km
+    
     @property
     def vehicle_type(self):
         return 'motorcycle'
 
     @property
     def vehicle_year(self):
-        return self.year
-
-    @property
-    def model(self):
-        return self.model_
+        return self._year
 
 
 class Truck(Vehicle):
-    def __init__(self, year, model, base_cost, passed_km):
+    """Truck"""
+    def __init__(self, year, model, base_cost):
         super().__init__()
         self.base_cost = base_cost
-        self.passed_km = passed_km
-        self.year = year
-        self.model_ = model
+        self._year = year
+        self.model = model
+
+    def purchase_price(self, km):
+        return self.base_cost - 0.1 * km
 
     @property
     def vehicle_type(self):
@@ -94,20 +86,19 @@ class Truck(Vehicle):
 
     @property
     def vehicle_year(self):
-        return self.year
-
-    @property
-    def model(self):
-        return self.model_
+        return self._year
 
 
 class Bus(Vehicle):
-    def __init__(self, year, model, base_cost, passed_km):
+    """Bus"""
+    def __init__(self, year, model, base_cost):
         super().__init__()
         self.base_cost = base_cost
-        self.passed_km = passed_km
-        self.year = year
-        self.model_ = model
+        self._year = year
+        self.model = model
+
+    def purchase_price(self, km):
+        return self.base_cost - 0.1 * km
 
     @property
     def vehicle_type(self):
@@ -115,17 +106,24 @@ class Bus(Vehicle):
 
     @property
     def vehicle_year(self):
-        return self.year
-
-    @property
-    def model(self):
-        return self.model_
+        return self._year
 
 
 if __name__ == '__main__':
 
-    bus = Bus(2012, 'Man', 120000, 10000)
-    bus.vehicle_type
+
+    bus = Bus(2012, 'Man', 120000)
+
+    print(bus.vehicle_type)
+    print(bus.vehicle_year)
+    print(bus.model)
+    print(bus.purchase_price(10000))
+    
+    print(
+        Vehicle.is_motorcycle(2),
+        Vehicle.is_motorcycle(4),
+    )
+
 
 
 
