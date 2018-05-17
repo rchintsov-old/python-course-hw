fbdc207d425f3643983b27bc7443a0c1a38ae3a1
Ruslan Chintsov <r.chintsov@gmail.com>
2018-05-17 14:45:11 +0300

    finished task

--- hw_6.2/task.py
+++ hw_6.2/task.py
@@ -1,11 +1,68 @@
-class Something:​
-    def __init__(self, x):​
-        self.x = x​
+class prop:
+    """
+    Property emulation class.
+    """
+    def __init__(self, fget=None, fset=None, fdel=None, doc=None):
+        self.fget = fget
+        self.fset = fset
+        self.fdel = fdel
+        if fget and not doc:
+            doc = fget.__doc__
+        self.__doc__ = doc
 
-    @prop​
-    def attr(self):​
-        return self.x ** 2​
+
+    def __get__(self, instance, a):
+        if not self.fget:
+            raise AttributeError("can't get the attribute")
+        return self.fget(instance)
+
+
+    def __set__(self, instance, value):
+        if not self.fset:
+            raise AttributeError("can't set the attribute")
+        return self.fset(instance, value)
+
+
+    def __delete__(self, instance):
+        if not self.fdel:
+            raise AttributeError("can't delete the attribute")
+        return self.fdel(instance)
+
+
+    def getter(self, fget):
+        self.fget = fget
+
+
+    def setter(self, fset):
+        self.fset = fset
+
+
+    def deleter(self, fdel):
+        self.fdel = fdel
+
+
+class Something:
+    """
+    Class for testing.
+    """
+    def __init__(self, x):
+        self.x = x
+
+
+    @prop
+    def attr(self):
+        return self.x ** 2
+
 
     @attr.setter
     def attr_setter(self, update):
-        return self.x = update
+        self.x = update
+
+
+if __name__ == '__main__':
+
+    s = Something(10)
+    print(s.attr)
+
+    s.attr = 3
+    print(s.attr)
