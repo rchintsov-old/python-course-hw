8a6edb41857370b3d21cbe3b45c4febe26041109
Ruslan Chintsov <r.chintsov@gmail.com>
2018-07-13 03:49:50 +0300

    Classes: property reengineering

--- hw_06.2/Property_emulation.py
+++ hw_06.2/Property_emulation.py
@@ -48,11 +48,9 @@ class Something:
     def __init__(self, x):
         self.x = x
 
-
     @prop
     def attr(self):
         return self.x ** 2
-
 
     @attr.setter
     def attr_setter(self, update):
